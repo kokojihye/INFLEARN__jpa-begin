2129e6d7045a29cbe3720da7f4d84e0c8d7b58ed
kokojihye <jihyevivianako@gmail.com>
2023-11-27 20:01:22 +0900

    예제4-상속관계 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -10,8 +10,11 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public abstract class Item {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ITEM_ID")
     private Long id;
 
     private String name;
