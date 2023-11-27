f1f5d16b5b64e536bacd7cb00d8a26e01200a0cc
kokojihye <jihyevivianako@gmail.com>
2023-11-27 15:15:27 +0900

    실전 예제1- 요구사항 분석과 기본 매핑

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -1,53 +1,20 @@
-package hellojpa;
+package jpabook;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import java.util.List;
-
-import static javax.persistence.Persistence.createEntityManagerFactory;
-
 public class JpaMain {
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+
         EntityTransaction tx = em.getTransaction();
-        //transaction begin
         tx.begin();
+
         try{
-            Team team1 = new Team();
-            team1.setName("Team A");
-            em.persist(team1);
-
-            Team team2 = new Team();
-            team2.setName("Team A");
-            em.persist(team2);
-
-            Member member = new Member();
-            member.setName("member1");
-            member.setTeam(team1);
-            em.persist(member);
-
-            System.out.println("======== DB 쿼리 확인 ========");
-            em.flush();
-            em.clear();
-            System.out.println("=============================");
-
-            //
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for(Member m : members){
-                System.out.println("m = " + m.getName());
-            }
-
-            System.out.println("======= before commit =======");
             tx.commit();
-            System.out.println("======= after commit =======");
-
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,2 +1,26 @@
-package jpabook.jpashop.domain;public class OrderItem {
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    private int orderPrice;
+
+    private int count;
+
 }
